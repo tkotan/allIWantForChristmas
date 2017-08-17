ed4637f6aae1808086e9b0e43422f1ef21390cd8
Susan <cho@Choui-MacBook-Pro-164.local>
2017-08-17 17:38:32 -0400

    Created wishlistController, Mongoose connections, static('public'), comments: Finalized server

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+//Declarations & Requires
 const express = require ('express');
 const app = express();
 const methodOverride = require ('method-override');
 const mongoose = require ('mongoose');
 const bodyParser = require ('body-parser');
 
+//Middleware
 app.use(bodyParser.json());
-mongoose.connect
+app.use(express.static('public'));
 
+//Controller
+const wishlistController = require('./controllers/wishlist.js');
+app.use('/wishlist', wishlistController);
 
+//Port
 const port = process.env.PORT || 3000;
 
+//Mongoose
+mongoose.connect('mongodb://localhost:27017/meancrud');
+mongoose.connection.once('open', ()=>{
+  console.log('Mongo connection SUCCESS')
+});
+
+//Listening
 app.listen(port, function(){
-  console.log('listening...');
+  console.log('Listening...');
 });
